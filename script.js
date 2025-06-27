a75c9b7a9ca4309be40348c7f7f714571b503b55
Chaddy20 <airamiano82@gmail.com>
2025-06-27 13:27:29 +0800

    files for my github portfolio

---
+++ script.js
@@ -0,0 +1,6 @@
+const h1name = document.getElementById("name");
+h1name.textContent = "HI IM FRANCIS IVAN RHEY";
+
+const age = document.querySelector(".age");
+age.textContent = "24 years old";
+Address.textContent = "Brgy, Saputan";
